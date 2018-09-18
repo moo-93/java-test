68bbd974833de19199348827d540566bbefb8dcb
moo <msh9303@naver.com>
2018-09-18 20:41:49 +0900

    메뉴기능 추가

--- java-test-project/src/main/java/App.java
+++ java-test-project/src/main/java/App.java
@@ -32,12 +32,55 @@ public class App {
     static Scanner keyIn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        inputMember();
-        printMember();
+        
+        while(true) {
+            String menu = promptMenu();
+            
+            if(menu.equals("1")) {
+                serviceStudentMenu();
+            } else if (menu.equals("0")) {
+                System.out.println("Bye!");
+                break;
+            }
+        }
         keyIn.close();
     }
 
-    static void printMember() {
+    private static void serviceStudentMenu() {
+        while(true) {
+            System.out.println("[list] or [add] or [quit]");
+            System.out.println("학생 관리 > ");
+
+            String command = keyIn.nextLine();
+            if(command.equals("list")) {
+                printMembers();
+            } else if(command.equals("add")) {
+                inputMember();
+            } else if(command.equals("quit")) {
+                return;
+            } else {
+                System.out.println("유효하지 않은 입력입니다.");
+            }
+        }
+    }
+    private static String promptMenu() {
+        while(true) {
+            System.out.println("[메뉴]");
+            System.out.println("1.학생관리 \t 2.매니저관리 \t 3.선생관리 \t 0.종료");
+
+            String menu = keyIn.nextLine();
+            switch(menu) {
+            case "1":
+            case "2":
+            case "3":
+            case "0":
+                return menu;
+            default:
+                System.out.println("유효하지 않은 입력입니다.");
+            }
+        }
+    }
+    static void printMembers() {
         for(int i = 0; i < index; i++) {
             System.out.printf("%s, %s, %s\n", members[i].getName(), members[i].getEmail(), members[i].getPassword());
         }
