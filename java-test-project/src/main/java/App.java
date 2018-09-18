7f5771ddf158b38d2cfdf4ecbd91ef912c778efd
moo <msh9303@naver.com>
2018-09-18 19:48:43 +0900

    배열을 이용하여 여러 회원의 정보 저장

--- java-test-project/src/main/java/App.java
+++ java-test-project/src/main/java/App.java
@@ -3,25 +3,34 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+        String[] name = new String[100];
+        String[] email = new String[100];
+        String[] password = new String[100];
+        int index = 0;
         
         Scanner keyIn = new Scanner(System.in);
-        
+
         while(true) {
-        System.out.print("이름  > ");
-        String name = keyIn.nextLine();
-        
-        System.out.print("이메일 > ");
-        String email = keyIn.nextLine();
-        
-        System.out.print("암호 > ");
-        String password = keyIn.nextLine();
-        
-        System.out.printf("%s, %s, %s\n", name, email, password);
-        System.out.println("continue?(Y/n)");
-        
-        String answer = keyIn.nextLine();
-        if(answer.toLowerCase().equals("n"))
-            break;
+            System.out.print("이름  > ");
+            name[index] = keyIn.nextLine();
+
+            System.out.print("이메일 > ");
+            email[index] = keyIn.nextLine();
+
+            System.out.print("암호 > ");
+            password[index] = keyIn.nextLine();
+
+            System.out.println("continue?(Y/n)");
+
+            index++;
+            
+            String answer = keyIn.nextLine();
+            if(answer.toLowerCase().equals("n"))
+                break;
         }
+        for(int i = 0; i < index; i++) {
+            System.out.printf("%s, %s, %s\n", name[i], email[i], password[i]);
+        }
+        keyIn.close();
     }
 }
